6b96acb378df4a5b2539cf54bfea60e83d14f77b
Harry <harryrock67@gmail.com>
2018-10-30 00:08:24 -0400

    Solved transition

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("scroll", e => {
-    let y = window.pageYOffset;
-    y > 400 ? (header.classList.add('headerIn')) : (header.classList.remove('headerIn'));
-})
+    let y = window.pageYOffset,
+        yPos = 80 - y / 8;
+    yPos > 20 ? header.style.top = yPos + 'vh' : 0;
+});
