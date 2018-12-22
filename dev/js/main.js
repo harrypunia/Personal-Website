fb36f34c330859f2c7e11bb07e31aced5ace5625
Harry <harryrock67@gmail.com>
2018-12-22 15:19:55 -0500

    New Parameters for Distort-text()

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,27 +6,38 @@ const openHeader = () => {
 }
 
 const distortText = (where, what) => {
-    let result = "",
-        delay = 0.0,
-        str = what.msg,
-        char = '';
+    let distortHTML = "",
+        opacity = what.type == 'distort_in' ? 0 : what.type == 'distort_out' ? 1 : 1,
+        bezier = what.type == 'distort_in' ? 'cubic-bezier(0, .41, .21, 1.01)' : what.type == 'distort_out' ? 'ease-in' : 'ease',
+        duration = what.duration == undefined ? .2 : what.duration,
+        speed = what.speed == undefined ? what.msg.length : what.speed;
 
-    for (let i = 0; i < str.length; i++) {
-        delay = ((i / 10) / 2) + .2;
-        char = (str[i] == "_") ? "&ensp;" : str[i];
-        result += '<div class="text__distort" style="animation: distort .4s cubic-bezier(0, .41, .21, 1.01); animation-fill-mode: forwards; animation-delay:' + delay + 's">' + char + '</div>';
+    for (let i = 0; i < what.msg.length; i++) {
+        let delay = ((i / speed) / 2) + what.delay,
+            str = what.msg[i].replace('_', '&ensp;');
+        what.delay == undefined ? what.delay = 0 : 0;
+        distortHTML += '<div style="opacity:' + opacity + '; animation: ' + what.type + ' ' + duration + 's ' + bezier + '; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>';
     }
-    typeof where == 'string' ? document.getElementById(where).innerHTML = result : document.getElementById(where.currentTarget.id).innerHTML = result;
+    document.getElementById(where).innerHTML = distortHTML;
 }
 
 window.addEventListener("load", () => {
     distortText('test', {
-        msg: 'Hello_there._Call_me'
+        msg: 'Hello_there._Call_me',
+        delay: .5,
+        type: 'distort_in',
+        duration: .4
     });
     distortText('test2', {
-        msg: 'Harry_Punia'
+        msg: 'Harry_Punia',
+        delay: .2,
+        type: 'distort_in',
+        duration: .4
     });
     distortText('test3', {
-        msg: 'Designer_•_Developer'
+        msg: 'Designer_•_Developer',
+        delay: .5,
+        type: 'distort_in',
+        duration: .4
     });
 });
