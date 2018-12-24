f6eab253aa9336f23da9af1872ab804cd95eb66b
Harry <harryrock67@gmail.com>
2018-12-24 18:25:48 -0500

    direction defined in distortText

--- dev/js/customHTML.js
+++ dev/js/customHTML.js
@@ -17,19 +17,20 @@ window.customElements.define('fade-scroll', FadeScroll);
 class DistortText extends HTMLElement {
     constructor() {
         super();
-        this.letters = '';
     }
     connectedCallback() {
         this.text = this.getAttribute('text');
-        this.delay = this.getAttribute('delay');
-        this.speed = this.getAttribute('speed');
-        this.hasAttribute('onload') ? this.distortText(this.text) : 0;
+        this.animation = this.getAttribute('dir');
+        this.delay = this.hasAttribute('delay') ? this.getAttribute('delay') : 0.2;
+        this.speed = this.hasAttribute('speed') ? this.getAttribute('speed') : this.text.length;
+        this.hasAttribute('onload') ? this.distortText() : this.hasAttribute('onclick') ? this.addEventListener('click', this.distortText) : 0;
     }
     distortText() {
+        this.letters = '';
         for (let i = 0; i < this.text.length; i++) {
             let delay = (i / parseInt(this.speed) / 2) + parseFloat(this.delay, 10),
                 str = this.text[i].replace('_', '&ensp;');
-            this.letters += '<div style="opacity: 0; animation: distort_in .2s ease-out; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>'
+            this.letters += '<div style="opacity: 0; animation: distort_' + this.animation + ' .2s ease-out; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>'
         }
         this.innerHTML = this.letters;
     }
