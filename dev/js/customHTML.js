f04ca8c1ff195104afe8001a9827a8eaa3726371
Harry <harryrock67@gmail.com>
2018-12-24 17:34:53 -0500

    custom elements for distort created

--- dev/js/customHTML.js
+++ dev/js/customHTML.js
@@ -16,8 +16,24 @@ window.customElements.define('fade-scroll', FadeScroll);
 
 class DistortText extends HTMLElement {
     constructor() {
-
+        super();
+        this.letters = '';
+    }
+    connectedCallback() {
+        this.text = this.getAttribute('text');
+        this.delay = this.getAttribute('delay');
+        this.hasAttribute('onload') ? this.distortText(this.text) : 0;
+    }
+    distortText() {
+        for (let i = 0; i < this.text.length; i++) {
+            let delay = (i / 2) + parseInt(this.delay, 10),
+                str = this.text[i].replace('_', '&ensp;');
+            console.log(typeof this.delay)
+            this.letters += '<div style="opacity: 0; animation: distort_in .2s ease-out; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>'
+        }
+        this.innerHTML = this.letters;
+        //'<div style="opacity:' + opacity + '; animation: ' + what.type + ' ' + duration + 's ' + bezier + '; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>';
     }
 }
 
-window.CustomElements.define('define-text', DefineText);
+window.customElements.define('distort-text', DistortText);
--- dev/js/main.js
+++ dev/js/main.js
@@ -7,40 +7,40 @@ const openHeader = () => {
     headerIsOpen ? (header.classList.remove('openHeader'), headerIsOpen = false, burger.classList.remove('convertBurger')) : (header.classList.add('openHeader'), headerIsOpen = true, burger.classList.add('convertBurger'));
 }
 
-const distortText = (where, what) => {
-    let distortHTML = "",
-        opacity = what.type == 'distort_in' ? 0 : what.type == 'distort_out' ? 1 : 1,
-        bezier = what.type == 'distort_in' ? 'cubic-bezier(0, .41, .21, 1.01)' : what.type == 'distort_out' ? 'ease-in' : 'ease',
-        duration = what.duration == undefined ? .2 : what.duration,
-        speed = what.speed == undefined ? what.msg.length : what.speed;
-
-    for (let i = 0; i < what.msg.length; i++) {
-        let delay = ((i / speed) / 2) + what.delay,
-            str = what.msg[i].replace('_', '&ensp;');
-        what.delay == undefined ? what.delay = 0 : 0;
-        distortHTML += '<div style="opacity:' + opacity + '; animation: ' + what.type + ' ' + duration + 's ' + bezier + '; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>';
-    }
-    document.getElementById(where).innerHTML = distortHTML;
-    document.getElementById(where).style.cssText = 'display: flex; justify-content: center';
-}
-
-window.addEventListener("load", () => {
-    distortText('test', {
-        msg: 'Hello_there._Call_me',
-        delay: .5,
-        type: 'distort_in',
-        duration: .4
-    });
-    distortText('test2', {
-        msg: 'Harry_Punia',
-        delay: .2,
-        type: 'distort_in',
-        duration: .4
-    });
-    distortText('test3', {
-        msg: 'Designer_•_Developer',
-        delay: .5,
-        type: 'distort_in',
-        duration: .4
-    });
-});
+//const distortText = (where, what) => {
+//    let distortHTML = "",
+//        opacity = what.type == 'distort_in' ? 0 : what.type == 'distort_out' ? 1 : 1,
+//        bezier = what.type == 'distort_in' ? 'cubic-bezier(0, .41, .21, 1.01)' : what.type == 'distort_out' ? 'ease-in' : 'ease',
+//        duration = what.duration == undefined ? .2 : what.duration,
+//        speed = what.speed == undefined ? what.msg.length : what.speed;
+//
+//    for (let i = 0; i < what.msg.length; i++) {
+//        let delay = ((i / speed) / 2) + what.delay,
+//            str = what.msg[i].replace('_', '&ensp;');
+//        what.delay == undefined ? what.delay = 0 : 0;
+//        distortHTML += '<div style="opacity:' + opacity + '; animation: ' + what.type + ' ' + duration + 's ' + bezier + '; animation-fill-mode: forwards; animation-delay:' + delay + 's">' + str + '</div>';
+//    }
+//    document.getElementById(where).innerHTML = distortHTML;
+//    document.getElementById(where).style.cssText = 'display: flex; justify-content: center';
+//}
+//
+//window.addEventListener("load", () => {
+//    distortText('test', {
+//        msg: 'Hello_there._Call_me',
+//        delay: .5,
+//        type: 'distort_in',
+//        duration: .4
+//    });
+//    distortText('test2', {
+//        msg: 'Harry_Punia',
+//        delay: .2,
+//        type: 'distort_in',
+//        duration: .4
+//    });
+//    distortText('test3', {
+//        msg: 'Designer_•_Developer',
+//        delay: .5,
+//        type: 'distort_in',
+//        duration: .4
+//    });
+//});
