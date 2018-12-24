e8bc24e1b1cbe6835c871cb1536f3ead0831500f
Harry <harryrock67@gmail.com>
2018-12-24 18:45:48 -0500

    check

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,3 +4,14 @@ const openHeader = () => {
 
     headerIsOpen ? (header.classList.remove('openHeader'), headerIsOpen = false, burger.classList.remove('convertBurger')) : (header.classList.add('openHeader'), headerIsOpen = true, burger.classList.add('convertBurger'));
 }
+
+window.onclick = () => {
+    let test = document.getElementById('hey'),
+        exit = test.getAttribute('exit');
+    console.log(exit);
+    if (exit == null || exit == false) {
+        test.setAttribute('exit', true);
+    } else {
+        test.setAttribute('exit', false)
+    }
+}
