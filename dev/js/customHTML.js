f11a1266a7f1cb0d86145b9af101048410bd79ef
Harry <harryrock67@gmail.com>
2019-01-08 17:41:31 -0500

    good night

--- dev/js/customHTML.js
+++ dev/js/customHTML.js
@@ -144,18 +144,19 @@ class SkillComponent extends HTMLElement {
         })
     }
     connectedCallback() {
-        let wrapper = document.createElement('div'),
-            text = document.createElement('p'),
+        this.wrapper = document.createElement('wrapper');
+        let text = document.createElement('p'),
             img = document.createElement('img'),
             _img = this.getAttribute('src'),
             alt = this.getAttribute('alt'),
             _text = this.getAttribute('text'),
             css = document.createElement('style');
-        wrapper.setAttribute('class', 'skill')
+        this.wrapper.setAttribute('class', 'skill')
         css.textContent = '.skill {flex-basis: 20%; margin: 20px 0}' +
             '.skill img{width: 20%; max-width: 100px; min-width: 50px; margin-left: 50%; transform: translate(-50%, -50%) rotate(30deg); opacity: 0;transition: transform .1s ease-out, opacity .2s ease-out}' +
             '.skill img:hover {transform: translate(-50%, 0) rotate(10deg) !important; transition: transform .1s ease-in}' +
             '.skill p {text-align: center; marign-top: 5px}' +
+            '.style[open] {opacity: 1 !important; transform: translate(-50%, 0); transition: transform .2s ease-in, opacity .2s ease-in;}' +
             '@media screen and (max-width: 800px) {' +
             '.skill {flex-basis: 50%}' +
             '.skill img:hover {transform: translate(-50%, 0) rotate(0) !important; cursor: default; transition: transform .1s ease-in}' +
@@ -163,20 +164,21 @@ class SkillComponent extends HTMLElement {
         text.textContent = _text;
         img.setAttribute('src', _img);
         this.shadow.appendChild(css);
-        this.shadow.appendChild(wrapper);
-        wrapper.appendChild(img);
-        wrapper.appendChild(text);
+        this.shadow.appendChild(this.wrapper);
+        this.wrapper.appendChild(img);
+        this.wrapper.appendChild(text);
     }
     static get observedAttributes() {
         return ['open']
     }
     attributeChangedCallback(name, oVal, nVal) {
         if (oVal != nVal) {
-            nVal == 'open' ? this.updateCss() : 0;
+            nVal == '' ? this.updateCss() : 0;
         }
     }
     updateCss() {
-        console.log('the drawer has been opened');
+        this.wrapper.setAttribute('open', '');
+        console.log('working');
     }
 }
 
