4a9ab8c502c89147a94b7019e10608e49f2af445
Harry <harryrock67@gmail.com>
2019-01-14 08:18:07 -0500

    added image wrapper to skills-component

--- dev/js/customHTML/ch-skills.js
+++ dev/js/customHTML/ch-skills.js
@@ -8,15 +8,18 @@ class SkillComponent extends HTMLElement {
     connectedCallback() {
         this.wrapper = document.createElement('wrapper');
         let text = document.createElement('p'),
+            imgWrapper = document.createElement('div'),
             img = document.createElement('img'),
             _img = this.getAttribute('src'),
             alt = this.getAttribute('alt'),
             _text = this.getAttribute('text'),
             css = document.createElement('style');
-        this.wrapper.setAttribute('class', 'skill')
+        this.wrapper.setAttribute('class', 'skill');
+        imgWrapper.setAttribute('class', 'img-wrapper');
         css.textContent = '.skill {flex-basis: 20%; margin: 20px 20px; display: flex; justify-content: center; align-items: center; flex-direction: column}' +
-            '.skill img{width: 20%; max-width: 100px; min-width: 50px; margin-left: 50%; transform: translate(-50%, -50%) rotate(30deg); opacity: 0;transition: transform .1s ease-out, opacity .2s ease-out}' +
-            '.skill img:hover {transform: translate(-50%, 0) rotate(20deg) !important; transition: transform .1s ease-in}' +
+            '.img-wrapper img{width: 20%; max-width: 100px; min-width: 50px; margin-left: 50%; opacity: 0}' +
+            '.img-wrapper {width: 250px; height: 400px; background: red; overflow: hidden; transform: translate(-50%, -50%); transition: transform .1s ease-out, opacity .2s ease-out}' +
+            '.img-wrapper:hover {transform: translate(-50%, 0) rotate(20deg) !important; transition: transform .1s ease-in}' +
             '.skill p {text-align: center; marign-top: 5px}' +
             '.skill[open] img {opacity: 1; transform: translate(-50%, 0); transition: transform .2s ease-in, opacity .2s ease-in;}' +
             '@media screen and (max-width: 800px) {' +
@@ -25,9 +28,10 @@ class SkillComponent extends HTMLElement {
             '}';
         text.textContent = _text;
         img.setAttribute('src', _img);
+        imgWrapper.appendChild(img);
         this.shadow.appendChild(css);
         this.shadow.appendChild(this.wrapper);
-        this.wrapper.appendChild(img);
+        this.wrapper.appendChild(imgWrapper);
         this.wrapper.appendChild(text);
     }
     static get observedAttributes() {
