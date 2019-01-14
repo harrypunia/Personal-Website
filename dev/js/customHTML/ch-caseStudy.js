4bc6c75a5aaa5a88a05d5ef32100deb547c5ad0f
Harry <harryrock67@gmail.com>
2019-01-14 09:59:23 -0500

    Added media css for case-study image and hovers

--- dev/js/customHTML/ch-caseStudy.js
+++ dev/js/customHTML/ch-caseStudy.js
@@ -7,6 +7,7 @@ class CaseStudy extends HTMLElement {
         this.wrapper = document.createElement('div');
         this.imgWrapper = document.createElement('div');
         this.img = document.createElement('img');
+        this.content = document.createElement('div');
         this.heading = document.createElement('h1');
         this.bio = document.createElement('p');
         this.css = document.createElement('style');
@@ -21,27 +22,29 @@ class CaseStudy extends HTMLElement {
         this.img.setAttribute('alt', this.alt);
         this.heading.textContent = this._heading;
         this.bio.textContent = this._bio;
+        this.content.appendChild(this.heading);
+        this.content.appendChild(this.bio);
         this.wrapper.setAttribute('class', 'wrapper');
         this.hasAttribute('right') ? this.imgWrapper.setAttribute('class', 'img-wrapper right') : this.imgWrapper.setAttribute('class', 'img-wrapper');
-        this.css.textContent = '.wrapper{position: relative; width: 100%; height: 400px; border: 1px solid white; margin: 100px 0; cursor: pointer}' +
-            '.img-wrapper {position: absolute; left: 0; top: -25%; width: 250px; height: 400px; box-shadow: 0 0 20px rgba(0, 0, 0, .4); transition: transform .2s; overflow: hidden; display: flex; justify-content: center; align-items: center}' +
+        this.css.textContent = '.wrapper{position: relative; width: 100%; height: 200px; border: 1px solid #333; margin: 200px 0; cursor: pointer; transition: background .2s, border .2s}' +
+            '.wrapper:hover {border: 1px solid white; background: #151515; transition: background .2s, border .2s}' +
+            '.img-wrapper {position: absolute; background: #a73030; top: -100px; width: 250px; height: 400px; box-shadow: 0 0 20px rgba(0, 0, 0, .4); transition: transform .2s; overflow: hidden; display: flex; justify-content: center; align-items: center}' +
             '.right {right: 0px !important; left: auto;}' +
-            '.img-warpper img{width: auto; height: 300px}' +
-            '.img-wrapper:hover img {transform: scale3d(0.6, 0.6, 1)}' +
+            'img{width: auto; height: 400px; transition: transform .2s ease-out}' +
+            '.img-wrapper:hover img {transform: scale3d(0.8, 0.8, 1); transition: transform .2s ease-out}' +
             '.wrapper h1{float: left; color: #333}' +
             '.wrapper p{float: left; color: #333}' +
-            '@media screen and (max-width: 800px) {' +
-            '.wrapper{height: 500px; margin: 200px 0;}' +
-            '.wrapper img {width: 33.33%; height: auto; min-width: 150px; left: 50%; transform: translateX(-50%)}' +
-            '.wrapper:hover img {transform: translateX(-50%) scale3d(1.1, 1.1, 1)}'
-        '}'
+            /*media*/
+            '@media screen and (max-width: 1000px) {' +
+            '.wrapper{height: 400px; margin: 200px 0;}' +
+            '.img-wrapper {width: 400px; height: 250px; left: 50%; transform: translateX(-50%)}' +
+            '.img-wrapper:hover img {transform: scale3d(0.8, 0.8, 1)}'
+        '}';
         this.imgWrapper.appendChild(this.img);
         this.shadow.appendChild(this.css);
         this.shadow.appendChild(this.wrapper);
         this.wrapper.appendChild(this.imgWrapper);
-        this.wrapper.appendChild(this.heading);
-        this.wrapper.appendChild(this.bio);
+        this.wrapper.appendChild(this.content);
     }
 }
-
 window.customElements.define('case-study', CaseStudy);
